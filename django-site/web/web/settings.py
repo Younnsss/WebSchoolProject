301c5d2dfbb61f796f1bbafb5d06e03a1c3a0d29
Younsss-ubuntu <younes.nown@gmail.com>
2024-10-03 17:13:12 +0200

    redoine

--- django-site/web/web/settings.py
+++ django-site/web/web/settings.py
@@ -155,7 +155,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+
+# Add these lines if they are not already present
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
